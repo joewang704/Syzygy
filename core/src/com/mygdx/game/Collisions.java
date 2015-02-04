563517cebc52a4e3ca4198af28c6688b9e9af63d
jdevanathan3 <jdevanathan3@gatech.edu>
2015-02-03 19:25:34 -0500

    Updated Collisions util class

---
+++ core/src/com/mygdx/game/Collisions.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Array;
+/*import com.mygdx.game.Bullet;
+import com.mygdx.game.Constants;
+import com.mygdx.game.User;*/
+
+import java.util.Iterator;
+
+/**
+ * Created by Jay on 2/3/2015.
+ */
+public class Collisions {
+    public static void removeBullets() {
+        Iterator<Bullet> uIter = User.userBullets.iterator();
+        while (uIter.hasNext()) {
+            Bullet bullet = uIter.next();
+            bullet.y += 200 * bullet.velocity.y * Gdx.graphics.getDeltaTime();
+            bullet.x += 200 * bullet.velocity.x * Gdx.graphics.getDeltaTime();
+            if (bullet.y + Constants.BULLET_HEIGHT < 0 || bullet.y > Constants.GAMESCREEN_HEIGHT) {
+                uIter.remove();
+            }
+        }
+    }
+    public static void enemyHits(Array<Enemy> enemies) {
+        for (Bullet bullet: User.userBullets) {
+            Iterator<Enemy> eIter = enemies.iterator();
+            while (eIter.hasNext()) {
+                if (bullet.overlaps(eIter.next())) {
+                    eIter.remove();
+                    User.userBullets.removeValue(bullet, true);
+                }
+            }
+        }
+    }
+}
--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -29,7 +29,7 @@ public class GameScreen implements Screen {
 
     //Shapes
     public User user;
-    Array<Enemy> enemies;
+    public Array<Enemy> enemies;
 
     //Utility
     private long lastSpawnTime;
@@ -117,7 +117,7 @@ public class GameScreen implements Screen {
         if (TimeUtils.nanoTime() - lastSpawnTime > 1000000000) spawnSlime();
 
         //moves bullets, removes bullets off screen
-        Iterator<Bullet> uIter = User.userBullets.iterator();
+        /*Iterator<Bullet> uIter = User.userBullets.iterator();
         while (uIter.hasNext()) {
             Bullet bullet = uIter.next();
             bullet.y += 200 * bullet.velocity.y * Gdx.graphics.getDeltaTime();
@@ -125,10 +125,11 @@ public class GameScreen implements Screen {
             if (bullet.y + Constants.BULLET_HEIGHT < 0 || bullet.y > Constants.GAMESCREEN_HEIGHT) {
                 uIter.remove();
             }
-        }
-
+        }*/
+        Collisions.removeBullets(); //removes bullets that have flown off the screen
+        Collisions.enemyHits(enemies); //removes enemies that have been hit by bullets
         //iterate through bullets and check if they collide with an enemy.
-        for (Bullet bullet: User.userBullets) {
+        /*for (Bullet bullet: User.userBullets) {
             Iterator<Enemy> eIter = enemies.iterator();
             while (eIter.hasNext()) {
                 if (bullet.overlaps(eIter.next())) {
@@ -136,7 +137,7 @@ public class GameScreen implements Screen {
                     User.userBullets.removeValue(bullet, true);
                 }
             }
-        }
+        }*/
 
         //move all enemies
         for (Enemy enemy: enemies) {

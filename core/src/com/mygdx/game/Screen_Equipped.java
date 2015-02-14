6f7ed118cd015daa06bda8a1f0baf9a1aa13f774
Lucas Della Bella <dellabella.lucas@gmail.com>
2015-02-13 19:34:17 -0500

    Started working on new rooms
    V0.0.2

--- core/src/com/mygdx/game/Screen_Equipped.java
+++ core/src/com/mygdx/game/Screen_Equipped.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 

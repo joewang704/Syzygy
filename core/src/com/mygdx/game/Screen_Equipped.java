aff7abf1f8b2b856488af37d19d37994d3e51dca
joewang704 <joewang704@gmail.com>
2015-02-20 02:55:03 -0500

    bug with equips not showing has been fixed, back button added to equip screen

--- core/src/com/mygdx/game/Screen_Equipped.java
+++ core/src/com/mygdx/game/Screen_Equipped.java
@@ -1,8 +1,13 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 /**
  * Created by Lucas on 2/9/2015.
@@ -19,11 +24,20 @@ public class Screen_Equipped extends Screen_MacroUI {
     ImageButton feet;
     ImageButton ring1, ring2;
 
+    private TextButton backButt;
+
     public Screen_Equipped(Syzygy game) {
         super(game);
-        /*equipment = new VerticalGroup();
+        equipment = new VerticalGroup();
         glovesAndChest = new HorizontalGroup();
 
+        //draw back button at top left corner
+        backButt = new TextButton("<", uiSkin);
+        backButt.setSize(Constants.GAMESCREEN_WIDTH/20, Constants.GAMESCREEN_HEIGHT/20);
+        backButt.setPosition(Constants.GAMESCREEN_WIDTH/70,
+                Constants.GAMESCREEN_HEIGHT - backButt.getHeight()
+                        - Constants.GAMESCREEN_HEIGHT/60);
+        Syzygy.stage.addActor(backButt);
 
         //make equipped item buttons with generic textures
         head = new ImageButton(new TextureRegionDrawable(new TextureRegion(
@@ -56,20 +70,33 @@ public class Screen_Equipped extends Screen_MacroUI {
         equipment.addActor(legs);
         equipment.addActor(feet);
 
-        horizontalGroup.addActor(equipment);*/
+        Syzygy.stage.addActor(equipment);
+        equipment.setFillParent(true);
     }
 
     /**
      * scales button to GS_HEIGHT / curHeight / 5
+     * Joe: the problem was that curHeight isnt set yet, setMinHeight sets the height
+     * and before that the height is undefined and shapes around the image
      * @param button to scale
      */
     private void scaleButton (ImageButton button) {
-        button.getStyle().imageUp.setMinWidth(Constants.GAMESCREEN_HEIGHT / button.getHeight() / 5);
-        button.getStyle().imageUp.setMinHeight(Constants.GAMESCREEN_HEIGHT / button.getHeight() / 5);
+        button.getStyle().imageUp.setMinWidth(Constants.GAMESCREEN_HEIGHT / 5);//button.getHeight() / 5);
+        button.getStyle().imageUp.setMinHeight(Constants.GAMESCREEN_HEIGHT / 4);//button.getHeight() / 5);
     }
-
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        if (backButt.isPressed()) {
+            game.getScreen().dispose();
+            game.setScreen(new Screen_Menu(game));
+        }
+    }
     @Override
     public void dispose() {
-
+        backButt.clear();
+        backButt.remove();
+        equipment.clear();
+        equipment.remove();
     }
 }
